73ea596d13e3b486c15f4f69cabffa99ad46a1b9
ashishfatnani <ashishfatnani3@gmail.com>
2020-05-18 23:20:50 +0530

    frontend done

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,29 @@
 import React from "react";
-import {BrowserRouter as Router, Route} from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "semantic-ui-css/semantic.min.css";
+import { AuthProvider } from "./context/auth";
+import AuthRoute from "./util/AuthRoute";
+
 import MenuBar from "./components/MenuBar";
 import { Container } from "semantic-ui-react";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import SinglePost from './pages/SinglePost';
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Container>
-        <MenuBar></MenuBar>
-        {/* <Route exact path ="/" component ={Home}></Route>
-        <Route exact path ="/login" component ={Login}></Route>
-        <Route exact path ="/register" component ={Register}></Route> */}
-        </Container>
-      </Router>
+      <AuthProvider>
+        <Router>
+          <Container>
+            <MenuBar></MenuBar>
+            <Route exact path="/" component={Home}></Route>
+            <Route exact path="/login" component={Login}></Route>
+            <Route exact path="/register" component={Register}></Route>
+            <Route exact path="/posts/:postId" component={SinglePost} />
+          </Container>
+        </Router>
+      </AuthProvider>
     </div>
   );
 }
